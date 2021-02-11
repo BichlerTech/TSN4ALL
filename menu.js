d3602ac11f1f628a3e073073e796e97433511cb3
BichlerTech <66249065+BichlerTech@users.noreply.github.com>
2021-02-11 21:01:29 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,19 @@
+class Menu {
+  constructor() {
+  }
+}
+
 class EthMenuItem {
   constructor() {
+    this.id = null;
+    this.name = "";
+    this.description = "";
+    this.version = "";
+    this.revision = "";
+    this.builddate = "";
+    this.image = "";
+    this.portcount = 1;
+    this.speed = [1];
+    this.userdata = null;
   }
 }
