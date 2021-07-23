30c02463518c202f3c138293c22ba96788e06655
hannesbichler <h.bichler@bichler.tech>
2021-07-23 21:49:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -403,7 +403,7 @@ SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformTo
       var xmlhttp = new XMLHttpRequest();
       xmlhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
-           document.getElementById("txtHint").innerHTML = this.responseText;
+	   alert(this.responseText);
          }
       };
       xmlhttp.open("GET", "index.php?q=" + str, true);
