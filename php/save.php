c8605fb21887a805366d4f785b383bd587c5c3a8
hannesbichler <h.bichler@bichler.tech>
2021-07-24 23:48:46 +0200

    Update save.php

--- php/save.php
+++ php/save.php
@@ -1,1 +1,10 @@
-
+<?php
+// Array with names
+$command = $_REQUEST["command"];
+$value = $_REQUEST["value"];
+//$q = "ich und du";
+if($command == "project")
+  file_put_contents ("config.dat", $value);
+else if ($command == "config")
+  file_put_contents ("config.dat", $value);
+?>
