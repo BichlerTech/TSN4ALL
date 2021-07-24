ae9fad80612cc72a316ea5aba89a8faff68717f7
hannesbichler <h.bichler@bichler.tech>
2021-07-24 20:57:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformTo
   };
 
 document.onkeydown = my_onkeydown_handler;
+window.onbeforeunload = my_onkeydown_handler;
+
 function my_onkeydown_handler(event) {
 	alert("F5");
     switch (event.keyCode) {
