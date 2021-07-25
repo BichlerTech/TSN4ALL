003a49096c858bfaaa9874b77f2064d0811582e8
hannesbichler <h.bichler@bichler.tech>
2021-07-26 00:07:44 +0200

    Update load.php

--- php/load.php
+++ php/load.php
@@ -4,7 +4,7 @@
 $command = $_REQUEST["command"];
 //$q = "ich und du";
 if($command == "project")
-  echo file_get_contents ("../projects/project.dat");
+  echo file_get_contents ("../projects/test1/proj.dat");
 else
   echo $req;
 ?>
